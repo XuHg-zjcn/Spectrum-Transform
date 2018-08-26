3110e470097719da2ba2d4c45925d7934d48d145
XuHg-zjcn <1687701765@qq.com>
2018-08-27 00:50:02 +0800

    make discriminator

--- blocks.py
+++ blocks.py
@@ -5,31 +5,43 @@ Created on Fri Aug 24 21:56:49 2018
 
 @author: xrj
 """
+import copy
 import tensorflow as tf
-#from nets.mobilenet import mobilenet_v2
+from nets.mobilenet import mobilenet_v2
 slim = tf.contrib.slim
+from nets.mobilenet import conv_blocks as ops
 
-def get_layer_num():
-    global layer_num
-    layer_num +=1
-    return layer_num
+D_V2_DEF = copy.deepcopy(mobilenet_v2.V2_DEF)
+D_V2_DEF['defaults'][(ops.expanded_conv,)].pop('normalizer_fn')
 
-def layer1x1(input_tensor, is_training):
+class layer_num(object):
+    
+    def __init__(self):
+        self.__num = 0
+    
+    @property
+    def get(self):
+        self.__num += 1
+        return self.__num
+    
+    def clear(self):
+        self.__num = 0
+
+def layer1x1(input_tensor, num_outputs, is_training, 
+             activation_fn=tf.nn.relu, normalizer_fn=slim.batch_norm):
     with tf.variable_scope('1x1'):
         net = slim.dropout(input_tensor, scope='Dropout', is_training=is_training)
         net = slim.conv2d(
-            net,
-            1280, [1, 1],
-            #activation_fn=None,
-            #normalizer_fn=None,
+            net, num_outputs, [1, 1], stride=1, padding='VALID', 
+            activation_fn=activation_fn,
+            normalizer_fn=normalizer_fn,
             biases_initializer=tf.zeros_initializer(),
             scope='Conv2d_1c_1x1')
     return net
 
 #blocks
-def up_block(input_tensor, from_mbnet2, num_outputs, 
+def up_block(input_tensor, from_mbnet2, num_outputs, counter,
              stride=None, lmul=None, fmul=None, **kwargs):
-    num_input = num_outputs * 2
     if lmul is None:
         lmul = 1
     if fmul is None:
@@ -38,39 +50,47 @@ def up_block(input_tensor, from_mbnet2, num_outputs,
         stride = 2
     num_input = input_tensor.get_shape().as_list()[3]
     num_from  = from_mbnet2.get_shape().as_list()[3]
-    with tf.variable_scope('up_block_%d'%get_layer_num()):
-        net = tf.identity(input_tensor)
+    with tf.variable_scope('up_block_%d'%counter.get):
+        net = tf.identity(input_tensor, name='input')
         if lmul != 0:
             net = convt1x1(net, num_input*lmul, scope='expand')
+            #tf.identity(net, name='expand_out')
             net = convt3x3(net, num_input*lmul, stride, scope='3x3', **kwargs)
+            #tf.identity(net, name='3x3_out')
         if fmul != 0:
-            pre = convt1x1(from_mbnet2, num_from*fmul, scope='pretreat')
+            pre = convt1x1(from_mbnet2, num_from*fmul, scope='preprocess')
+            #tf.identity(net, name='preprocess_out')
         net = tf.concat([net, pre], 3)
-        net = convt1x1(net, num_outputs, scope='output', activation_fn=None)
-        net = tf.identity(net)
+        net = convt1x1(net, num_outputs, scope='down', activation_fn=None)
+        net = tf.identity(net, name='output')
     return net
 
-def norm_block(input_tensor, num_outputs, mul=None):
+def norm_block(input_tensor, num_outputs, counter, mul=None):
     if mul is None:
         mul = 6
     num_input = input_tensor.get_shape().as_list()[3]
-    with tf.variable_scope('norm_block_%d'%get_layer_num()):
-        input_tensor = tf.identity(input_tensor)
-        net = convt1x1(input_tensor, num_input*mul, scope='expand')
-        net = convsep3(net, None         , scope='3x3')
-        net = convt1x1(net, num_outputs  , scope='output', activation_fn=None)
+    with tf.variable_scope('norm_block_%d'%counter.get):
+        net = tf.identity(input_tensor, name='input')
+        net = convt1x1(net, num_input*mul, scope='expand')
+        #net = tf.identity(net, name='expand_out')
+        net = convsep3(net, None, scope='3x3')
+        #net = tf.identity(net, name='3x3_out')
+        net = convt1x1(net, num_outputs, scope='down', activation_fn=None)
         if(num_input == num_outputs): #check add
+            #net = tf.identity(net, name='down_out')
             net += input_tensor
-        net = tf.identity(net)
+        net = tf.identity(net, name='output')
     return net
 
-def only_up(input_tensor, num_outputs):
+def only_up(input_tensor, num_outputs, counter):
+    global numer
     #num_input = input_tensor.get_shape().as_list()[3]
-    with tf.variable_scope('up_without_from_%d'%get_layer_num()):
-        input_tensor = tf.identity(input_tensor)
-        net = convt1x1(input_tensor, num_outputs, scope='expand')
-        net = convsep3(net, None         , scope='3x3')
-        net = tf.identity(net)
+    with tf.variable_scope('up_without_from_%d'%counter.get):
+        net = tf.identity(input_tensor, name='input')
+        net = convt1x1(net, num_outputs, scope='expand')
+        #net = tf.identity(input_tensor, name='expand_out')
+        net = convsep3(net, None, scope='3x3')
+        net = tf.identity(net, name='output')
     return net
                                    
 def convt1x1(input_tensor, num_outputs, **kwargs):
@@ -90,40 +110,123 @@ def convsep3(input_tensor, num_outputs, **kwargs):
                                  depth_multiplier=1, stride=1, **kwargs);
 
 def gen_transpose(input_tensor, mbnet2):
+    counter = layer_num()
     with tf.variable_scope('transpose'):
         with slim.arg_scope([slim.conv2d, slim.conv2d_transpose, slim.separable_conv2d], 
                             padding='SAME', 
                             activation_fn=tf.nn.relu6, 
                             normalizer_fn=slim.batch_norm,
                             trainable=True):
-            net = tf.identity(input_tensor)
-            net = tf.tile(net, [1,7,7,1])
-            net =   up_block(net, mbnet2['layer_18'], 160, 
+            net =tf.identity(input_tensor, name='intput')
+            net =    tf.tile(net, [1,7,7,1])
+            net =   up_block(net, mbnet2['layer_18'], 160, counter,
                                  stride=2, fmul=5, lmul=0, padding='VALID')  # 18
-            net = norm_block(net, 80)                                # 17
-            net = norm_block(net, 80)                                                             
-            net = norm_block(net, 80)                                # 15
-            net =   up_block(net, mbnet2['layer_14'], 48, 
+            net = norm_block(net, 80, counter)                               # 17
+            net = norm_block(net, 80, counter)                               # 16
+            net = norm_block(net, 80, counter)                               # 15
+            net =   up_block(net, mbnet2['layer_14'], 48, counter, 
                                  stride=2, fmul=2, lmul=2)                   # 14
-            net = norm_block(net, 48)                                # 13
-            net = norm_block(net, 48)                                # 12
-            net =   up_block(net, mbnet2['layer_11'], 32, 
+            net = norm_block(net, 48, counter)                               # 13
+            net = norm_block(net, 48, counter)                               # 12
+            net =   up_block(net, mbnet2['layer_11'], 32, counter, 
                                  stride=1, fmul=2, lmul=2)                   # 11
-            net = norm_block(net, 32)                                # 10
-            net = norm_block(net, 32)                                # 9
-            net = norm_block(net, 32)                                # 8
-            net =   up_block(net, mbnet2['layer_7'], 16, 
+            net = norm_block(net, 32, counter)                               # 10
+            net = norm_block(net, 32, counter)                               # 9
+            net = norm_block(net, 32, counter)                               # 8
+            net =   up_block(net, mbnet2['layer_7'], 16, counter, 
                                  stride=2, fmul=3, lmul=3)                   # 7
-            net = norm_block(net, 16)                                # 6
-            net = norm_block(net, 16)                                # 5
-            net =   up_block(net, mbnet2['layer_4'], 16, 
+            net = norm_block(net, 16, counter)                               # 6
+            net = norm_block(net, 16, counter)                               # 5
+            net =   up_block(net, mbnet2['layer_4'], 16, counter, 
                                  stride=2, fmul=3, lmul=3)                   # 4
-            net = norm_block(net, 16)                                # 3
-            net =   up_block(net, mbnet2['layer_2'], 8, 
+            net = norm_block(net, 16, counter)                               # 3
+            net =   up_block(net, mbnet2['layer_2'], 8, counter, 
                                  stride=2, fmul=3, lmul=3)                   # 2
-            net =    only_up(net, 16)                                # 1
+            net =    only_up(net, 16, counter)                               # 1
             net = slim.conv2d_transpose(net, 1, [3, 3], 
-                                            stride=2, padding='SAME', scope='irout_3', 
-                                            activation_fn=slim.relu, normalizer_fn=None)
-            net = tf.identity(net)
+                                            stride=2, padding='SAME', scope='irout_3')
+            net = tf.identity(net, name='output')
     return net
+
+def den_block(input_tensor, num_output, counter, stride=1, mul=6, exp=None):
+    num_input = input_tensor.get_shape().as_list()[3]
+    with tf.variable_scope('den_block%i'%counter.get):
+        input_tensor = tf.identity(input_tensor, name='input')
+        if exp == False:
+            net = slim.conv2d(input_tensor, num_input*mul, [1,1], stride=1, scope='expand')
+            net = tf.identity(net, name='expand_out')
+        else:
+            net = input_tensor
+        net = slim.separable_conv2d(net, num_input*mul, [3,3], 
+                                 depth_multiplier=1, stride=1, scope='3x3')
+        net = tf.identity(net, name='3x3_out')
+        net = slim.conv2d(net, num_output, [1,1], stride=stride, scope='down')
+        if num_input == num_output and stride==1:
+            net = tf.identity(net, name='down_out')
+            net += input_tensor
+        net = tf.identity(input_tensor, name='output')
+        return net
+        
+def den_net(input_tensor):
+    counter = layer_num()
+    endpoints = []
+    with tf.variable_scope('den_net'):
+        with slim.arg_scope([slim.conv2d, slim.separable_conv2d], 
+                            padding='SAME', 
+                            activation_fn=tf.nn.relu6, 
+                            normalizer_fn=None,
+                            trainable=True):
+            net = tf.identity(input_tensor, name='input')
+            net=slim.conv2d(net, 16, [3,3], stride=2)
+            net = den_block(net,  8, counter, stride=1, mul=1, exp=False)
+            endpoints.append(net)
+            net = den_block(net, 16, counter, stride=2, mul=6)
+            net = den_block(net, 16, counter, stride=1, mul=6)
+            net = den_block(net, 16, counter, stride=2, mul=6)
+            endpoints.append(net)
+            net = den_block(net, 16, counter, stride=1, mul=6)
+            net = den_block(net, 16, counter, stride=1, mul=6)
+            net = den_block(net, 32, counter, stride=2, mul=6)
+            endpoints.append(net)
+            net = den_block(net, 32, counter, stride=1, mul=6)
+            net = den_block(net, 32, counter, stride=1, mul=6)
+            net = den_block(net, 32, counter, stride=1, mul=6)
+            endpoints.append(net)
+            net = den_block(net, 48, counter, stride=1, mul=6)
+            net = den_block(net, 48, counter, stride=1, mul=6)
+            net = den_block(net, 48, counter, stride=1, mul=6)
+            endpoints.append(net)
+            net = den_block(net, 80, counter, stride=2, mul=6)
+            net = den_block(net, 80, counter, stride=1, mul=6)
+            net = den_block(net, 80, counter, stride=1, mul=6)
+            endpoints.append(net)
+            net = den_block(net,160, counter, stride=1, mul=6)
+            net=slim.conv2d(net,1280, [1,1], stride=1)
+            net=tf.nn.avg_pool(net, [1, 7, 7, 1], [1, 1, 1, 1], padding='VALID')
+            endpoints.append(net)
+            net = tf.identity(input_tensor, name='ontput')
+    return net, endpoints
+
+def den_out(input_list1,input_list2):
+    counterA = layer_num()
+    assert len(input_list1) == len(input_list2)
+    inlen = len(input_list1)
+    lo = 0
+    for i in range(inlen):
+        with tf.variable_scope('in_%d'%counterA.get):
+            counterN = layer_num()
+            net = tf.abs(input_list1[i] - input_list2[i])
+            dchs = net.get_shape().as_list()[3]
+            if(dchs>7):
+                net = den_block(net, dchs, counterN, stride=2, mul=6)
+            net = den_block(net, dchs, counterN, stride=1, mul=6)
+            net = den_block(net, dchs, counterN, stride=1, mul=6)
+            net = den_block(net, dchs*2, counterN, stride=1, mul=6)
+            assert net.get_shape().as_list()[1] == net.get_shape().as_list()[2]
+            patch = net.get_shape().as_list()[1]
+            net = slim.avg_pool2d(net, [patch, patch], stride=1, padding='VALID')
+            net = tf.identity(net, name='output')
+            net = layer1x1(net, 1, is_training=True, 
+                           activation_fn=tf.nn.relu, normalizer_fn=None)
+        lo += net
+    return lo
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ Created on Thu Aug 23 21:13:16 2018
 
 @author: xrj
 """
-
 #import numpy as np
 import tensorflow as tf
 #from tensorflow.python import debug as tf_debug
@@ -17,28 +16,58 @@ import blocks
 
 slim = tf.contrib.slim
 sess = tf.InteractiveSession()
-
-
-
+batch_size = 10
+random_dim = 5
 #bulid
-transpose_layers = []
 #gen_input = tf.placeholder(shape=(None,224,224,3),dtype=tf.float32)
-def G(input_tensor, reuse=False):
-    with tf.variable_scope('G', reuse=reuse):
-        net, mbnet2 = mobilenet_v2.mobilenet_v2_050(input_tensor,num_classes=0)
-        net = blocks.layer1x1(net, is_training=True)
+def G(input_tensor):
+    with tf.variable_scope('G', reuse=tf.AUTO_REUSE):
+        net, mbnet2 = mobilenet_v2.mobilenet_v2_050(input_tensor, num_classes=0)
+        net = blocks.layer1x1(net, 1280, is_training=True, 
+                              activation_fn=tf.nn.relu, normalizer_fn=slim.batch_norm)
         net = blocks.gen_transpose(net, mbnet2)
     return net
     
+
 #den_input = tf.placeholder(shape=(None,224,224,1),dtype=tf.float32)
-def D(input_tensor, reuse=False):
-    with tf.variable_scope('D'):
-        net, mbnet2 = mobilenet_v2.mobilenet_v2_050(input_tensor,num_classes=0)
-        net = blocks.layer1x1(net, is_training=True)
+def D(input_tensor1, input_tensor2):
+    with tf.variable_scope('D', reuse=tf.AUTO_REUSE):
+#        with tf.contrib.slim.arg_scope(mobilenet_v2.training_scope()):
+#            net, mbnet2 = mobilenet_v2.mobilenet_v2_050(input_tensor, num_classes=0)
+        _, endpoints1 = blocks.den_net(input_tensor1)
+        _, endpoints2 = blocks.den_net(input_tensor1)
+        net = blocks.den_out(endpoints1, endpoints2)
     return net
 
-#Dloss = 
-#Gloss = 
-saver = tf.train.Saver()
+# get the variables list is empty
+# copy from https://github.com/bojone/gan/blob/master/mnist_gangp.py
+real_IR = tf.placeholder(tf.float32, shape=[1, 224, 224, 1])
+Vis = tf.placeholder(tf.float32, shape=[1, 224, 224, 3])
+gen_IR = G(Vis)
+
+eps = tf.random_uniform([1, 224, 224, 1], minval=0., maxval=1.)
+X_inter = tf.add(eps*real_IR, (1. - eps)*gen_IR, name='X_inter')
+grad = tf.gradients(D(X_inter, real_IR), [X_inter])[0]
+
+grad_norm = tf.sqrt(tf.reduce_sum((grad)**2, axis=(1, 2, 3)))
+grad_pen = 10 * tf.reduce_mean(tf.nn.relu(grad_norm - 1.), name='grad_pen')
+
+D_gen_IR = D(gen_IR, real_IR)
+D_gen_IR = tf.identity(D_gen_IR, name='D_gen_IR')
+D_loss = grad_pen - D_gen_IR
+G_loss = D_gen_IR
+#loss = D_loss + G_loss
+
+D_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='D')
+G_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='G')
+G_mbnet2_varlist = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='G/MobilenetV2/')
+
+G_solver = tf.train.AdamOptimizer(1e-4, 0.5, name='Adam_G').minimize(G_loss, var_list=G_variables)
+D_solver = tf.train.AdamOptimizer(1e-4, 0.5, name='Adam_D').minimize(D_loss, var_list=D_variables)
+#op = tf.train.AdamOptimizer(1e-4).minimize(loss)
+
+#saver = tf.train.Saver(G_mbnet2_varlist)
 merged_summary_op = tf.summary.merge_all()
 summary_writer = tf.summary.FileWriter('./mobilenetv2-test', sess.graph)
+checkpoint = './mobilenet_v2_0.5_224/mobilenet_v2_0.5_224.ckpt'
+#saver.restore(sess, checkpoint)
