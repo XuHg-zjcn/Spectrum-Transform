4b957d28438728b9cad7294f46f1ad030112d203
XuHg-zjcn <1687701765@qq.com>
2018-11-17 20:48:17 +0800

    apply license

--- image-preprocess.py
+++ image-preprocess.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 27 05:37:53 2018
+   Copyright 2018 Xu Ruijun
 
-@author: xrj
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 """
 import math
 import os
@@ -16,22 +26,24 @@ import matplotlib.pyplot as plt
 from matplotlib import widgets
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--vis", help="visible image dir")
-parser.add_argument("-i", "--ir", help="IR image dir")
+parser.add_argument("-v", "--vis", help="visible image dir", 
+                    default='../../TT/hres_vis')
+parser.add_argument("-r", "--ir", help="IR image dir", 
+                    default='../../TT/hres_tir')
+parser.add_argument("-i", "--input", help="input npy file")
 parser.add_argument("-o", "--out", help="output file")
 parser.add_argument("-s", "--start", help="start of file name",
                     type=int)
+parser.add_argument("-f", "--first", help="first image num in npy file",
+                    type=int, default=0)
 args = parser.parse_args()
 
 vis_base=args.vis
 ir_base=args.ir
 
 lnpixel = 120*0.014/math.tan(math.radians(17))
-
-nlst = []
-for i in os.listdir(vis_base):
-     nlst.append(int(i[4:-4]))
-
+nlst = np.load('nlst.npy')
+scans = np.load(args.input)
 
 '''
 l25m: the list of 25 point test of each image shape(nimage, 25)
@@ -40,7 +52,7 @@ l45m: shape(nimage, 20)
 logtemp: [RAWlargmax, 'str', yshift, xshift]
 '''
 class align():
-    def __init__(self, filename, start_num):
+    def __init__(self, filename, start_num, first_img):
         timestruct = time.localtime()
         strtime = time.strftime('%Y-%m-%d %H:%M:%S', timestruct)
         self.outfn = filename
@@ -48,94 +60,96 @@ class align():
         self.outf.write('-----'+strtime+'-----\n')
         self.outf.close()
         self.img_num = start_num
-        self.l25m = []
-        self.l45mn = []
-        self.l45m = []
+        self.first_img = first_img
         self.logtemp = []
         self.yshiftNpix = None
         self.xshiftNpix = 0
-        
+    
+    #print(num) is a speed test
     def next_img(self):
         self.yshiftNpix = None
         self.xshiftNpix = 0
         self.outf = open(self.outfn, 'a')
         self.outf.write('FLIR%d'%(nlst[self.img_num]))
-        if len(self.logtemp) == 2:
-            self.outf.write(' %2d %8s\n'%tuple(self.logtemp))
-        elif len(self.logtemp) == 4:
-            self.outf.write(' %2d %8s %2d %2d\n'%tuple(self.logtemp))
+        if len(self.logtemp) == 3:
+            self.outf.write(' %2d %2d %8s\n'%tuple(self.logtemp))
+        elif len(self.logtemp) == 5:
+            self.outf.write(' %2d %2d %8s %2d %2d\n'%tuple(self.logtemp))
         elif len(self.logtemp) == 0:
             pass
         else:
-            raise ValueError('len logtemp not 2 or 4')
+            raise ValueError('len logtemp not 3 or 5')
         self.outf.close()
         self.logtemp = []
         self.img_num +=1
         n = nlst[self.img_num]
-        
+        self.scan = scans[self.img_num - self.first_img]
+        #print(1)
         vis_name = os.path.join(vis_base, 'FLIR'+str(n)+'.jpg')
         ir_name = os.path.join(ir_base, 'FLIR'+str(n)+'.png')
         vis_img = Image.open(vis_name)
         vis_img = vis_img.resize((427,320), Image.ANTIALIAS)
         self.vis_img = np.array(vis_img).astype(np.float32)
         ir_img = np.array(Image.open(ir_name)).astype(np.float32)
-        
+        #print(2)
         vis_G = np.gradient(gaussian_filter(self.vis_img, 1), axis=(0,1))
         ir_G = np.gradient(ir_img, axis=(0,1))
-    
-        vis_dy = vis_G[0].mean(axis=2)
-        vis_dx = vis_G[1].mean(axis=2)
+        #print(3)
+        vis_dy = vis_G[0]
+        vis_dx = vis_G[1]
         vis_r = np.sqrt(vis_dx*vis_dx + vis_dy*vis_dy)
+        vis_r = np.linalg.norm(vis_r, axis=2)
         ir_dy = ir_G[0]
         ir_dx = ir_G[1]
         ir_r = np.sqrt(ir_dx*ir_dx + ir_dy*ir_dy)
-    
-        vis_g = gaussian_filter(vis_r, 1)
-        ir_g = gaussian_filter(ir_r, 1)
-        self.vis_gnorm = vis_g/np.percentile(vis_g,99)
-        self.ir_gnorm = ir_g/np.percentile(ir_g,99)
-        l25 = []
-        for j in range(25):
-            l25.append((vis_g[35+j:35+j+240, 53:53+320] * ir_g).mean())
-        self.l25m.append(l25)
-        lmax = np.array(l25).argmax()
-        if lmax > 20:
-            self.l45mn.append(self.img_num)
-            for j in range(20):
-                l25.append((vis_g[60+j:60+j+240, 53:53+320] * ir_g).mean())
-            self.l45m.append(l25[25:])
-            lmax = np.array(l25).argmax()
-            linex = range(-5,40)
-        else:
-            linex = range(-5,20)
-        self.logtemp.append(lmax - 5)        
-        if(self.yshiftNpix == lmax - 5):
-            self.update_show()
-        else:
-            self.yshiftNpix = lmax - 5
-            yshift.set_val(lmax - 5)
+        self.vis_norm = vis_r/np.percentile(vis_r, 99)
+        self.ir_norm = ir_r/np.percentile(ir_r, 99)
+        #print(4)
+        self.yshiftNpix, self.xshiftNpix = divmod(self.scan.argmax(), 40)
+        self.yshiftNpix -= 20
+        self.xshiftNpix -= 20
+        #print(5)
+        self.logtemp.append(self.yshiftNpix)
+        self.logtemp.append(self.xshiftNpix)
         # subplot(223)
-        '''vis_gnorm_crop = self.vis_gnorm[35+lmax:35+lmax+240, 53:53+320]
-        grad_img = np.stack((vis_gnorm_crop,self.ir_gnorm,vis_gnorm_crop), axis=2)
+        vis_norm_crop = self.vis_norm[40+self.yshiftNpix:
+                                      40+self.yshiftNpix+240, 
+                                      53+self.xshiftNpix:
+                                      53++self.xshiftNpix+320]
+        grad_img = np.stack((vis_norm_crop,self.ir_norm,vis_norm_crop), axis=2)
         grad_show.clear()
         grad_show.imshow(grad_img)
         # subplot(221)
-        vis_crop = self.vis_img[35+lmax:35+lmax+240, 53:53+320]/255.0
+        vis_crop = self.vis_img[40+self.yshiftNpix:
+                                40+self.yshiftNpix+240, 
+                                53+self.xshiftNpix:
+                                53++self.xshiftNpix+320]/255.0
         vis_show.clear()
-        vis_show.imshow(vis_crop)'''
+        vis_show.imshow(vis_crop)
         # subplot(222)
         ir_show.clear()
         ir_show.imshow(ir_img)
         # subplot(224)
-        line.clear()
-        line.plot(linex, l25)
-        plt.draw()
+        scan_show.clear()
+        scan_show.imshow(self.scan, 
+                         extent=(-20.5,19.5,39.5,-20.5), cmap='gist_ncar')
+        
+        scan_show.scatter(self.xshiftNpix, self.yshiftNpix, color='red')
+        #print(6)
         try:
-            show_l = lnpixel / (lmax - 5)
+            show_yl = lnpixel / (self.yshiftNpix)
         except ZeroDivisionError:
-            show_l = float('inf')
-        namebox.set_val("FLIR%d"%n)
-        ym.set_val('%.2fm'%show_l)
+            show_yl = float('inf')
+        
+        try:
+            show_xl = lnpixel / (self.xshiftNpix)
+        except ZeroDivisionError:
+            show_xl = float('inf')
+        
+        plt.draw()
+        print('FLIR%d'%n)
+        print('y %2d %.2f'%(self.yshiftNpix, show_yl))
+        print('x %2d %.2f'%(self.xshiftNpix, show_xl))
     
     def pass_click(self, event):
         self.logtemp.append('pass')
@@ -147,8 +161,8 @@ class align():
         
     def change_value_click(self, event):
         self.logtemp.append('change')
-        self.logtemp.append(self.yshiftNpix)
-        self.logtemp.append(self.xshiftNpix)
+        #self.logtemp.append(int(input('yshift:')))
+        #self.logtemp.append(int(input('xshift:')))
         self.next_img()
         
     def update_show(self):
@@ -159,41 +173,20 @@ class align():
         vis_show.clear()
         vis_show.imshow(vis_crop)
         
-        vis_gnorm_crop = self.vis_gnorm[35+n35:35+n35+240, 53+xsp:53+xsp+320]
-        grad_img = np.stack((vis_gnorm_crop,self.ir_gnorm,vis_gnorm_crop), axis=2)
+        vis_norm_crop = self.vis_norm[35+n35:35+n35+240, 53+xsp:53+xsp+320]
+        grad_img = np.stack((vis_norm_crop,self.ir_norm,vis_norm_crop), axis=2)
         grad_show.clear()
         grad_show.imshow(grad_img)
-    
-    def submit_y(self, text):
-        try:
-            self.yshiftNpix = int(text)
-        except ValueError:
-            print("text can't to int")
-        else:
-            self.update_show()
-            try:
-                show_l = lnpixel / self.yshiftNpix
-            except ZeroDivisionError:
-                show_l = float('inf')
-            ym.set_val('%.2fm'%show_l)
-            
-    def submit_x(self, text):
-        try:
-            self.xshiftNpix = int(text)
-        except ValueError:
-            print("text can't to int")
-        else:
-            self.update_show()
 
 if args.start is None:
     start_num = -1
 else:
     start_num = np.where(np.array(nlst) == args.start)[0][0]
-aligner  = align(args.out, start_num)
+aligner  = align(args.out, start_num, args.first)
 vis_show = plt.subplot(221)
 ir_show = plt.subplot(222)
 grad_show = plt.subplot(223)
-line = plt.subplot(224)
+scan_show = plt.subplot(224)
 pass_ax = plt.axes([0.81, 0.01, 0.09, 0.05])
 pass_button = widgets.Button(pass_ax, 'pass')
 pass_button.on_clicked(aligner.pass_click)
@@ -205,23 +198,6 @@ not_pass_button.on_clicked(aligner.not_pass_click)
 change_value_ax = plt.axes([0.71, 0.05, 0.05, 0.03])
 change_value_button = widgets.Button(change_value_ax, 'change value')
 change_value_button.on_clicked(aligner.change_value_click)
-
-namebox_ax = plt.axes([0.01, 0.01, 0.06, 0.05])
-namebox = widgets.TextBox(namebox_ax, 'file', 'FLIRxxxx')
-
-yshiftbox_ax = plt.axes([0.08, 0.01, 0.02, 0.02])
-yshift = widgets.TextBox(yshiftbox_ax, '', 'xx')
-yshift_submit =  yshift.on_submit(aligner.submit_y)
-
-ym_ax = plt.axes([0.11, 0.01, 0.04, 0.02])
-ym = widgets.TextBox(ym_ax, '', 'x.xxm')
-
-xshiftbox_ax = plt.axes([0.08, 0.05, 0.02, 0.02])
-xshift = widgets.TextBox(xshiftbox_ax, '', 'xx')
-xshift_submit =  xshift.on_submit(aligner.submit_x)
-
-xm_ax = plt.axes([0.11, 0.05, 0.04, 0.02])
-xm = widgets.TextBox(xm_ax, '', 'x.xxm')
-
+# TextBox.set_val is slowly, I no longer use it
 aligner.next_img()
 plt.show()
